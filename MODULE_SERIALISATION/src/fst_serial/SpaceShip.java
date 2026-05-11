954df1f4a501c549328492585ab6ffcecdc5e776
Dinormisimus <dinowow123@gmail.com>
2026-05-11 17:16:13 +0200

    Update

---
+++ MODULE_SERIALISATION/src/fst_serial/SpaceShip.java
@@ -0,0 +1,35 @@
+package fst_serial;
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SpaceShip {
+    private String spaceShipName;
+    private int id;
+    private static int cntID;
+    private LocalDate firstMissionDate;
+
+    public SpaceShip(String name){
+        this.spaceShipName = name;
+        this.id = cntID++;
+        this.firstMissionDate = genRandomFiestMissionDate();
+    }
+
+    private LocalDate genRandomFiestMissionDate(){
+        LocalDate startDate = LocalDate.of(2000,12,27);
+        long longStartDate = startDate.toEpochDay();
+        LocalDate endDate = LocalDate.of(2090,12,27);
+        long longEndDate = endDate.toEpochDay();
+        long randomDateBetween = ThreadLocalRandom.current().nextLong(longStartDate, longEndDate);
+        LocalDate randomDate = LocalDate.ofEpochDay(randomDateBetween);
+        return randomDate;
+    }
+
+    @Override
+    public String toString() {
+        return "SpaceShip{" +
+                "spaceShipName='" + spaceShipName + '\'' +
+                ", id=" + id +
+                ", firstMissionDate=" + firstMissionDate +
+                '}';
+    }
+}
---
+++ MODULE_SERIALISATION/src/fst_serial/TestApp.java
@@ -0,0 +1,11 @@
+package fst_serial;
+
+public class TestApp {
+
+    static void main() {
+        SpaceShip spaceShip1 = new SpaceShip("Apollo");
+        SpaceShip spaceShip2 = new SpaceShip("Enterprise");
+        System.out.println(spaceShip1);
+        System.out.println(spaceShip2);
+    }
+}
